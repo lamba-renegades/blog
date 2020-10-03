0532914ab3fd70adb1ea776fa762a245c50df587
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-03 17:42:01 -0500

    adds eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-one-expression-per-line': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};

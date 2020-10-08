80d687498eadf9273bfd6b0d4e63e42b763df15f
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-07 20:21:19 -0500

    adds minor refactor to modules and tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    'jest/global': true,
+    jest: true,
   },
   extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parser: '@typescript-eslint/parser',
--- lib/posts.ts
+++ lib/posts.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
+import markdownToHtml from './markdown-to-html';
+
 const POSTS_FOLDER = 'content';
 
 const postsDirectory = path.join(process.cwd(), POSTS_FOLDER);
@@ -12,26 +14,27 @@ export const getPostSlugs = () => {
   return postFiles.map((file) => file.replace(/\.md$/, ''));
 };
 
-export const getPostBySlug = (slug: string) => {
+export const getPostBySlug = async (slug: string) => {
   const postPath = path.join(postsDirectory, `${slug}.md`);
   const fileContent = fs.readFileSync(postPath, 'utf-8');
 
-  return matter(fileContent);
+  const result = matter(fileContent);
+  const content = await markdownToHtml(result.content);
+  return { ...result, content };
 };
 
-export const getAllPosts = (options: { limit: number }) => {
+export const getAllPosts = async (options: { limit: number }) => {
   const { limit = 10 } = options;
 
   const slugs = getPostSlugs();
 
-  const posts = slugs
-    .map(getPostBySlug)
+  const posts = await Promise.all(slugs.map(getPostBySlug));
+
+  return posts
     .map((post) => ({
       ...post,
       data: { ...post.data, date: new Date(post.data.date) },
     }))
     .sort((post1, post2) => (post1.data.date < post2.data.date ? -1 : 1))
     .slice(0, limit);
-
-  return posts;
 };

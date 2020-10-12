ff059103382e4a70b35a8cefc85a81f68866d7ba
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-10-12 18:49:35 -0500

    adds some mock posts to work for now

---
+++ lib/mocks/posts.tsx
@@ -0,0 +1,92 @@
+export default [
+  {
+    slug: 'post-1',
+    data: {
+      title: 'Learn how to pre-render pages using ssg with next.js',
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+      coverImage: '/assets/posts/hello-world.png',
+      author: {
+        name: 'Foo Bar',
+        picture: '/assets/authors/foo.png',
+      },
+      date: '2020-03-16T05:35:07.322Z',
+    },
+    content: '',
+  },
+  {
+    slug: 'post-2',
+    data: {
+      title: 'Learn how to pre-render pages using ssg with next.js',
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+      coverImage: '/assets/posts/hello-world.png',
+      author: {
+        name: 'Foo Bar',
+        picture: '/assets/authors/foo.png',
+      },
+      date: '2020-03-16T05:35:07.322Z',
+    },
+    content: '',
+  },
+  {
+    slug: 'post-3',
+    data: {
+      title: 'Learn how to pre-render pages using ssg with next.js',
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+      coverImage: '/assets/posts/hello-world.png',
+      author: {
+        name: 'Foo Bar',
+        picture: '/assets/authors/foo.png',
+      },
+      date: '2020-03-16T05:35:07.322Z',
+    },
+    content: '',
+  },
+  {
+    slug: 'post-4',
+    data: {
+      title: 'Learn how to pre-render pages using ssg with next.js',
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+      coverImage: '/assets/posts/hello-world.png',
+      author: {
+        name: 'Foo Bar',
+        picture: '/assets/authors/foo.png',
+      },
+      date: '2020-03-16T05:35:07.322Z',
+    },
+    content: '',
+  },
+  {
+    slug: 'post-5',
+    data: {
+      title: 'Learn how to pre-render pages using ssg with next.js',
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+      coverImage: '/assets/posts/hello-world.png',
+      author: {
+        name: 'Foo Bar',
+        picture: '/assets/authors/foo.png',
+      },
+      date: '2020-03-16T05:35:07.322Z',
+    },
+    content: '',
+  },
+  {
+    slug: 'post-6',
+    data: {
+      title: 'Learn how to pre-render pages using ssg with next.js',
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+      coverImage: '/assets/posts/hello-world.png',
+      author: {
+        name: 'Foo Bar',
+        picture: '/assets/authors/foo.png',
+      },
+      date: '2020-03-16T05:35:07.322Z',
+    },
+    content: '',
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,47 +3,49 @@ import Link from 'next/link';
 
 import PostType from '../lib/types/post';
 import Layout from '../components/Layout';
-import * as postsService from '../lib/posts';
+// import * as postsService from '../lib/posts';
+import postMocks from '../lib/mocks/posts';
 
 export default function Home({ posts }: { posts: Array<PostType> }) {
   return (
     <div>
-      <Layout />
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Layout>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <h1>Title</h1>
+        <main>
+          <h1>Title</h1>
 
-        <h2>Content</h2>
-        <hr />
-        {posts.map((post) => (
-          <div key={post.slug}>
-            <h3>
-              <Link href={`/posts/${post.slug}`}>
-                <a>{post.data.title}</a>
-              </Link>
-            </h3>
-            <span>{post.data.date}</span>
-            <span>{post.data.author.name}</span>
-            <p>{post.data.excerpt}</p>
-          </div>
-        ))}
-      </main>
+          <h2>Content</h2>
+          <hr />
+          {posts.map((post) => (
+            <div key={post.slug}>
+              <h3>
+                <Link href={`/posts/${post.slug}`}>
+                  <a>{post.data.title}</a>
+                </Link>
+              </h3>
+              <span>{post.data.date}</span>
+              <span>{post.data.author.name}</span>
+              <p>{post.data.excerpt}</p>
+            </div>
+          ))}
+        </main>
 
-      <footer>footer...</footer>
+        <footer>footer...</footer>
+      </Layout>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const posts = await postsService.getAllPosts({ limit: 10 });
+  // const posts = await postsService.getAllPosts({ limit: 10 });
 
   return {
     props: {
-      posts,
+      posts: postMocks,
     },
   };
 }
